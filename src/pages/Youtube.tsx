9cfb4fbac8052affff983c504880b947c4e90154
Marius ROBERT <mariusrobert380@gmail.com>
2023-04-23 00:37:05 +0200

    added dropzone and improved colors

--- src/pages/Youtube.tsx
+++ src/pages/Youtube.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import MainHeader from "../components/MainHeader";
-import {MantineProvider} from "@mantine/core";
+import {Center, MantineProvider, Text} from "@mantine/core";
+import {Dropzone} from "@mantine/dropzone";
+import {IconPhoto} from "@tabler/icons-react";
+import {useMantineTheme} from "@mantine/core";
 
 export default function Youtube() {
   return (
@@ -10,8 +13,24 @@ export default function Youtube() {
             primaryColor: "red"
           }}
       >
-        <div className="App" style={{height:"100vh"}}>
+        <div className="App" style={{height: "100vh", backgroundColor: useMantineTheme().colors.dark[4]}}>
           <MainHeader/>
+          <Center>
+            <Dropzone
+                onDrop={(file) => console.log(file)}
+                style={{width: 600}}
+                radius={"lg"}
+            >
+              <Dropzone.Idle>
+                <IconPhoto size="3.2rem" stroke={1.5} color={useMantineTheme().colors.dark[1]}/>
+              </Dropzone.Idle>
+              <div>
+                <Text mt={"sm"} size="xl" fw={500} inline color={useMantineTheme().colors.dark[1]}>
+                  Drag your history.json file here or click to select it
+                </Text>
+              </div>
+            </Dropzone>
+          </Center>
         </div>
       </MantineProvider>
   )
